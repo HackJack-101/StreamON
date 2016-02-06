fe2ce91ce37315e8e1f9a8fe097b25515ae40ea6
Jack3113 <jack03113@gmail.com>
2016-02-06 18:05:02 +0100

    Subtitute URLs in miniplayer

--- app/launch.js
+++ app/launch.js
@@ -20,46 +20,46 @@
  */
 
 chrome.app.runtime.onLaunched.addListener(function () {
-    chrome.app.window.create('window.html', {
-        id: "miniplayer",
-        innerBounds: {
-            width: 832,
-            height: 468
-        }
-    },
-            function (createdWindow) {
-                var win = createdWindow.contentWindow;
-                win.onload = function () {
+	chrome.app.window.create('window.html', {
+		id: "miniplayer",
+		innerBounds: {
+			width: 832,
+			height: 468
+		}
+	},
+			function (createdWindow) {
+				var win = createdWindow.contentWindow;
+				win.onload = function () {
 
-                    win.document.querySelector('#submit').value = chrome.i18n.getMessage("submit");
-                    win.document.querySelector('#alwaysOnTopLabel').innerHTML = chrome.i18n.getMessage("alwaysOnTop");
-                    win.document.querySelector('#alwaysOnTop').addEventListener('change', function () {
-                        var current = chrome.app.window.get("miniplayer");
-                        current.setAlwaysOnTop(current.contentWindow.document.querySelector('#alwaysOnTop').checked);
-                    });
+					win.document.querySelector('#submit').value = chrome.i18n.getMessage("submit");
+					win.document.querySelector('#alwaysOnTopLabel').innerHTML = chrome.i18n.getMessage("alwaysOnTop");
+					win.document.querySelector('#alwaysOnTop').addEventListener('change', function () {
+						var current = chrome.app.window.get("miniplayer");
+						current.setAlwaysOnTop(current.contentWindow.document.querySelector('#alwaysOnTop').checked);
+					});
 
-                    var title = win.document.querySelector('#title');
-                    title.innerHTML = chrome.i18n.getMessage("inputURL");
+					var title = win.document.querySelector('#title');
+					title.innerHTML = chrome.i18n.getMessage("inputURL");
 
-                    var form = win.document.querySelector('#formURL');
-                    form.addEventListener('submit', function (e)
-                    {
-                        e.preventDefault();
-                        var url = win.document.querySelector('#url').value;
-                        for (var i in modules)
-                        {
-                            if (modules[i].check(url))
-                            {
-                                url = modules[i].getEmbedURL(url);
-                                break;
-                            }
-                        }
-                        win.document.querySelector('#home').style.display = "none";
-                        var webview = win.document.createElement('webview');
-                        webview.setAttribute("src", url);
-                        win.document.querySelector('#content').appendChild(webview);
-                    });
-                };
-            }
-    );
+					var form = win.document.querySelector('#formURL');
+					form.addEventListener('submit', function (e)
+					{
+						e.preventDefault();
+						var url = substitute(win.document.querySelector('#url').value);
+						for (var i in modules)
+						{
+							if (modules[i].check(url))
+							{
+								url = modules[i].getEmbedURL(url);
+								break;
+							}
+						}
+						win.document.querySelector('#home').style.display = "none";
+						var webview = win.document.createElement('webview');
+						webview.setAttribute("src", url);
+						win.document.querySelector('#content').appendChild(webview);
+					});
+				};
+			}
+	);
 });
--- app/message.js
+++ app/message.js
@@ -21,57 +21,57 @@
 
 
 String.prototype.hash = function () {
-    var hash = 0;
-    if (this.length == 0)
-        return hash;
-    for (i = 0; i < this.length; i++) {
-        var char = this.charCodeAt(i);
-        var hash = ((hash << 5) - hash) + char;
-        var hash = hash & hash;
-    }
-    return "hash" + hash;
+	var hash = 0;
+	if (this.length == 0)
+		return hash;
+	for (i = 0; i < this.length; i++) {
+		var char = this.charCodeAt(i);
+		var hash = ((hash << 5) - hash) + char;
+		var hash = hash & hash;
+	}
+	return "hash" + hash;
 }
 
 
 chrome.runtime.onMessageExternal.addListener(function (msg) {
-    var windowID = msg.url.hash();
-    if (chrome.app.window.get(windowID))
-        chrome.app.window.get(windowID).focus();
-    chrome.app.window.create('window.html', {
-        id: windowID,
-        innerBounds: {
-            width: 832,
-            height: 468
-        },
-        frame: {color: '#000000'}
-    }, function (createdWindow) {
-        var win = createdWindow.contentWindow;
-        win.addEventListener("load", function () {
-            win.document.querySelector('#home').style.display = "none";
-            var webview = win.document.createElement('webview');
+	var windowID = msg.url.hash();
+	if (chrome.app.window.get(windowID))
+		chrome.app.window.get(windowID).focus();
+	chrome.app.window.create('window.html', {
+		id: windowID,
+		innerBounds: {
+			width: 832,
+			height: 468
+		},
+		frame: {color: '#000000'}
+	}, function (createdWindow) {
+		var win = createdWindow.contentWindow;
+		win.addEventListener("load", function () {
+			win.document.querySelector('#home').style.display = "none";
+			var webview = win.document.createElement('webview');
 
-            var url = msg.url;
-            if (msg.resolution)
-            {
-                for (var i in modules)
-                {
-                    if (modules[i].check(url))
-                    {
-                        url = modules[i].getEmbedURL(url);
-                        break;
-                    }
-                }
-            }
-            webview.setAttribute("src", url);
+			var url = substitute(msg.url);
+			if (msg.resolution)
+			{
+				for (var i in modules)
+				{
+					if (modules[i].check(url))
+					{
+						url = modules[i].getEmbedURL(url);
+						break;
+					}
+				}
+			}
+			webview.setAttribute("src", url);
 
-            win.document.querySelector('#content').appendChild(webview);
-            win.document.querySelector('#alwaysOnTopLabel').innerHTML = chrome.i18n.getMessage("alwaysOnTop");
-            win.document.querySelector('#alwaysOnTop').addEventListener('change', function () {
-                var id = windowID;
-                var current = chrome.app.window.get(id);
-                current.setAlwaysOnTop(current.contentWindow.document.querySelector('#alwaysOnTop').checked);
-                current.drawAttention();
-            });
-        });
-    });
+			win.document.querySelector('#content').appendChild(webview);
+			win.document.querySelector('#alwaysOnTopLabel').innerHTML = chrome.i18n.getMessage("alwaysOnTop");
+			win.document.querySelector('#alwaysOnTop').addEventListener('change', function () {
+				var id = windowID;
+				var current = chrome.app.window.get(id);
+				current.setAlwaysOnTop(current.contentWindow.document.querySelector('#alwaysOnTop').checked);
+				current.drawAttention();
+			});
+		});
+	});
 });
